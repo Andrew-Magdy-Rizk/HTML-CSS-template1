c217fba6e85c70759c48764a2dae0db49da85828
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2023-09-25 15:59:25 +0300

    add Script

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function navgation() {
-  document.getElementById("nav").style.display = "block";
-}
+let listLink = document.querySelector(".header .links");
+let myNav = document.querySelector(".header .links ul");
+let mySpan = document.querySelector(".header .links .icon span:nth-child(2)");
+
 function closeNav() {
   document.getElementById("nav").style.display = "none";
+  mySpan.style.width = "75%";
 }
+
+listLink.addEventListener("click", () => {
+  if (myNav.style.display === "none") {
+    myNav.style.display = "block";
+    mySpan.style.width = "100%";
+  } else {
+    myNav.style.display = "none";
+    mySpan.style.width = "75%";
+  }
+});
